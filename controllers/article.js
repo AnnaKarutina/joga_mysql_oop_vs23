d05188a218ce4ecc20700c71020845e4adb4a8a7
Anna Karutina <anna.karutina@khk.ee>
2024-09-06 11:06:41 +0300

    Displayed an article by slug using the article oop model

--- controllers/article.js
+++ controllers/article.js
@@ -9,7 +9,12 @@ class articleController {
     async getAllArticles(req, res) {
         this.articles = await articleModel.findAll()
         res.status(201).json({articles: this.articles})
-    }   
+    } 
+    
+    async getArticleBySlug(req, res) {
+        const article = await articleModel.findOne(req.params.slug)
+        res.status(201).json({article: article})
+    } 
 } 
 
 module.exports = new articleController()
